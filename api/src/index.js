bb5da8d140f3653ccf33928451fb46ea5eb459e4
Mark Rawls <markrawls96@gmail.com>
2020-10-05 17:40:13 -0400

    Refer to the Chrome instance by IP

--- api/src/index.js
+++ api/src/index.js
@@ -1,20 +1,29 @@
 import express from 'express';
 import puppeteer from 'puppeteer-core';
+import DNS from 'dns';
+
+const dns = DNS.promises;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true}));
+const browserHost = process.env.BROWSER_HOST || 'chrome';
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post('/', async (req, res) => {
-  const browser = await puppeteer.connect({browserURL: 'http://chrome:3001'});
+  const lookup = await dns.lookup(browserHost);
+
+  const browser = await puppeteer.connect({ browserURL: `http://${lookup.address}:3001` });
   const page = await browser.newPage();
+
   await page.setContent(req.body);
   const pdf = await page.pdf({ format: 'a4' });
-  await browser.close();
+  await page.close();
+
   res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
   res.send(pdf);
-})
+});
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));

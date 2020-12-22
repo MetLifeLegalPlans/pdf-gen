e75574307e9a19ba1ad68daf93c95a4c7b73db23
JCog <jordancogan4@gmail.com>
2020-12-22 18:39:21 -0500

    add test for invalid id, change index to document_id, check for content_type

--- pdfgen/tests.py
+++ pdfgen/tests.py
@@ -36,11 +36,12 @@ EXPECTED_METADATA = [{"test_anchor": [75.0, 75.0]}]
 
 class TestPdfgenViews(APITestCase):
     def test_convert(self):
-        # make sure convert returns a valid pdf
+        """make sure convert returns a valid pdf"""
         test_html = "\n".join(TEST_LINES)
         response = self.client.post(reverse("pdfgen:convert"), {"data": test_html})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.content, bytes)
+        self.assertEqual(response["Content-Type"], "application/pdf")
 
     def test_convert_with_metadata(self):
         # make sure we get an index back from convert-with-metadata
@@ -49,15 +50,21 @@ class TestPdfgenViews(APITestCase):
             reverse("pdfgen:convert-with-metadata"), {"data": test_html}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        index = response.json()["id"]
+        document_id = response.json()["id"]
 
         # make sure metadata returns the correct anchor locations
-        response = self.client.get(reverse("pdfgen:metadata", args=[index]))
+        response = self.client.get(reverse("pdfgen:metadata", args=[document_id]))
         anchors = response.json()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertListEqual(anchors, EXPECTED_METADATA)
 
         # make sure doc returns a valid pdf
-        response = self.client.get(reverse("pdfgen:doc", args=[index]))
+        response = self.client.get(reverse("pdfgen:doc", args=[document_id]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response, FileResponse)
+
+        # test for invalid id
+        response = self.client.get(reverse("pdfgen:metadata", args=[document_id + 1]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        response = self.client.get(reverse("pdfgen:doc", args=[document_id + 1]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- pdfgen/urls.py
+++ pdfgen/urls.py
@@ -7,7 +7,7 @@ app_name = "pdfgen"
 urlpatterns = [
     path("convert/", views.convert, name="convert"),
     path("convert-with-metadata/", views.convert_with_metadata, name="convert-with-metadata"),
-    path("metadata/<int:index>/", views.metadata, name="metadata"),
-    path("doc/<int:index>/", views.doc, name="doc"),
+    path("metadata/<int:document_id>/", views.metadata, name="metadata"),
+    path("doc/<int:document_id>/", views.doc, name="doc"),
     path("health/", views.health, name="health"),
 ]
